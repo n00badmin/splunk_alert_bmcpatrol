b3af59b661a1552a803bc5956496301c22d9b677
n00badmin <inthen00blab@gmail.com>
2016-09-18 01:57:35 +0000

    more scripting

--- alert_bmcpatrol/bin/bmcPatrol.py
+++ alert_bmcpatrol/bin/bmcPatrol.py
@@ -4,6 +4,9 @@
 #Splunk Custom Alert Action for BMC Patrol
 #Send dynamic alerts via the MSEND command
 #https://docs.bmc.com/docs/display/public/btsim96/mposter+and+msend+syntax
+#Matthew Modestino 09.2016
+#Please feel free to leave it better than you found it!
+#https://github.com/n00badmin/splunk_alert_bmcpatrol
 
 import sys
 import re
@@ -11,32 +14,36 @@ import subprocess
 import json
 import csv
 import gzip
-from collections import OrderedDict
 
+
+def bmcPatrol():
+    try:
+        #get json ouptut from splunk modular alert - See alert_actions.conf
+        payload = json.loads(sys.stdin.read())
+        config = payload.get('configuration', dict())
+        splunkServer = payload.get('server_host')
+        splunkURI = payload.get('server_uri')
+        splunkApp = payload.get('app')
+        splunkSearch = payload.get('search_name')
+        resultsLink = payload.get('results_link')
+        bmcPatrolTarget = config.get('serverip')
+        port = str(config.get('port'))
+        message = config.get('message')
+        severity = config.get('severity')
+        host = config.get('host')
+
+#Thanks Manny! outfile.write( json.dumps(body))
+#json,dumps({'a': "b", 'c': "d"}, indent=4) 
+#sort_keys=true for adding more cpu usage to your script too
+
+#use contents of body to dynamically populate, then execute the MSEND CLI command
+
+#msend -n @bmcPatrolTarget:port#mc -a EVENT –r severity -m message -b "mc_host=host; mc_tool_class=SPLUNK-ENTERPRISE; mc_object=$source; mc_object_owner=$team; mc_tool=$splunksearch; mc_tool_uri=$UriOfSplunkReportIfAvailableORESULTS"
+ 
+ 
 #Check args
 if __name__ == "__main__":
     if len(sys.argv) < 2 or sys.argv[1] != "--execute":
          print >> sys.stderr, "FATAL Unsupported execution mode (expected --execute flag)"
-         sys.exit(1) 
-    
-    try:
-        settings = json.loads(sys.stdin.read())
-        body = OrderedDict(
-            sid=settings.get('sid'),
-            search_name=settings.get('search_name'),
-            app=settings.get('app'),
-            owner=settings.get('owner'),
-            results_link=settings.get('results_link'),
-            result=settings.get('result')
-        )
-
-#use contents of body to dynamically populate, then execute the MSEND CLI command
-
-#msend -n @msend.host.com:1830#mc -a EVENT –r CRITICAL -m “Message passed by user in gui” -b "mc_host=$host; mc_tool_class=SPLUNK-ENTERPRISE; mc_object=$source; mc_object_owner=$team; mc_tool=$SearchName; mc_tool_uri=$UriOfSplunkReportIfAvailableORRESULTS"
- 
- 
-#-a Class                                 Sends an object of class Class
-#-r Severity                           Sets the event severity value to the Severity specified
-#-b SlotSetValue Adds SlotSetValue settings (format: "slot=value;...")
-#-m Message                       Sets event message to the specified Message text
- 
+         sys.exit(1)
+    bmcPatrol() 
