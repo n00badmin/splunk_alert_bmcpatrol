ea2b93ab5ced72cf536f57a5c4a26a7599a61dfe
n00badmin <inthen00blab@gmail.com>
2016-09-18 02:07:03 +0000

    moaar scripting

--- alert_bmcpatrol/bin/bmcPatrol.py
+++ alert_bmcpatrol/bin/bmcPatrol.py
@@ -5,7 +5,7 @@
 #Send dynamic alerts via the MSEND command
 #https://docs.bmc.com/docs/display/public/btsim96/mposter+and+msend+syntax
 #Matthew Modestino 09.2016
-#Please feel free to leave it better than you found it!
+#Please feel free to leave it better than you found it, I know just enough to be dangerous!
 #https://github.com/n00badmin/splunk_alert_bmcpatrol
 
 import sys
@@ -21,6 +21,7 @@ def bmcPatrol():
         #get json ouptut from splunk modular alert - See alert_actions.conf
         payload = json.loads(sys.stdin.read())
         config = payload.get('configuration', dict())
+        host = payload.get('host') 
         splunkServer = payload.get('server_host')
         splunkURI = payload.get('server_uri')
         splunkApp = payload.get('app')
@@ -30,7 +31,7 @@ def bmcPatrol():
         port = str(config.get('port'))
         message = config.get('message')
         severity = config.get('severity')
-        host = config.get('host')
+        team = config.get('team')
 
 #Thanks Manny! outfile.write( json.dumps(body))
 #json,dumps({'a': "b", 'c': "d"}, indent=4) 
@@ -38,7 +39,7 @@ def bmcPatrol():
 
 #use contents of body to dynamically populate, then execute the MSEND CLI command
 
-#msend -n @bmcPatrolTarget:port#mc -a EVENT –r severity -m message -b "mc_host=host; mc_tool_class=SPLUNK-ENTERPRISE; mc_object=$source; mc_object_owner=$team; mc_tool=$splunksearch; mc_tool_uri=$UriOfSplunkReportIfAvailableORESULTS"
+#msend -n @bmcPatrolTarget:port#mc -a EVENT –r severity -m message -b "mc_host=host; mc_tool_class=SPLUNK-ENTERPRISE; mc_object=source; mc_object_owner=team; mc_tool=splunksearch; mc_tool_uri=splunkSearch"
  
  
 #Check args
